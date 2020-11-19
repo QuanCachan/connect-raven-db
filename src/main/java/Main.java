4d377bcdcc7a2d0f7c1c7de224979ee10bc1ef22
QuanCachan <mqtran.cachan@gmail.com>
2020-11-19 23:41:50 +0100

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import entity.Inventory;
+import entity.Zone;
+import net.ravendb.client.documents.DocumentStore;
+import net.ravendb.client.documents.session.IDocumentSession;
+
+public class Main {
+    public static void main(String[] args) {
+        DocumentStore store = DocumentStoreHolder.getStore();
+        try (IDocumentSession session = store.openSession()) {
+            /*Inventory inventory = new Inventory();
+            inventory.setName("Entrepote");
+
+            session.store(inventory);                            // Assign an 'Id' and collection (Inventories)
+            // and start tracking an entity
+
+            Zone zone = new Zone();
+            zone.setName("Storage");
+            zone.setInventory(inventory.getId());
+
+            session.store(zone);                             // Assign an 'Id' and collection (Zones)
+            // and start tracking an entity
+
+            session.saveChanges();                              // Send to the Server
+            // one request processed in one transaction*/
+
+            Zone loadedZone = session.load(Zone.class, "zones/1-A");
+            System.out.println(loadedZone);
+            System.out.println("Name: "+ loadedZone.getName()
+                    +" , Inventory: " + loadedZone.getInventory());
+        }
+    }
+}
---
+++ src/main/java/entity/Zone.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Zone {
+    private String name;
+    private String inventory;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(String inventory) {
+        this.inventory = inventory;
+    }
+}
